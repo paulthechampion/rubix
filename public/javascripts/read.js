40694f9985439dab926b2aa71e2683e457cd9a55
paulthechampion <enwerempaulo@gmail.com>
2020-11-12 13:04:06 -0500

    canvas style

---
+++ public/javascripts/read.js
@@ -0,0 +1,85 @@
+
+const url= '<%-book.pdfPath%>'
+
+
+
+let pdfDoc = null
+    pageNum =1
+    pageIsRendering = false
+    pageNumIsPending = null
+
+const scale = 3
+    canvas = document.querySelector('#pdf-render')
+    ctx = canvas.getContext('2d')
+
+//render the page
+const renderPage= num =>{
+    pageIsRendering = true;
+
+    //Get page
+    pdfDoc.getPage(num).then(page=>{
+    
+    // Set scale
+    const viewport = page.getViewport({scale})
+    canvas.height= viewport.height
+    canvas.width = viewport.width
+
+    const renderCtx ={
+        canvasContext: ctx,
+        viewport
+    }
+    
+    page.render(renderCtx).promise.then(()=>{
+        pageIsRendering = false;
+        
+        if(pageNumIsPending !== null){
+            renderPage(pageNumIsPending)
+            pageNumIsPending =null;
+        }
+   
+    })
+
+    // Output current page
+    document.querySelector('#page-num').textContent= num;
+    })
+}
+
+// Check for pages rendering
+const queueRenderPage = num =>{
+    if(pageIsRendering){
+        pageNumIsPending =num;
+    }else{
+        renderPage(num)
+    }
+}
+
+//show Prev Page
+const showPrevPage= ()=>{
+    if(pageNum <= 1){
+        return
+    }
+    pageNum--
+    queueRenderPage(pageNum)
+}
+
+//show Next Page
+const showNextPage= ()=>{
+    if(pageNum >= pdfDoc.numPages){
+        return
+    }
+    pageNum++
+    queueRenderPage(pageNum)
+}
+
+// Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ =>{
+    pdfDoc= pdfDoc_
+    
+    document.querySelector('#page-count').textContent = pdfDoc.numPages
+
+    renderPage(pageNum)
+})
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage)
+document.querySelector('#next-page').addEventListener('click', showNextPage)
