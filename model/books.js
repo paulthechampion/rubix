b87b488ef00ca46264a65073b7f15eea42a04175
paulthechampion <enwerempaulo@gmail.com>
2021-02-15 17:34:35 -0500

    book-cover virtual

--- model/books.js
+++ model/books.js
@@ -51,8 +51,7 @@ const bookSchema = new mongoose.Schema({
 
 bookSchema.virtual('coverImagePath').get(function(){
     if(this.coverImage != null && this.coverImageType != null){
-        return `data:${this.coverImageType}; charset=utf-8; base64,
-         ${this.coverImage.toString('base64')}`
+        return `data:${this.coverImageType}; charset=utf-8; base64,${this.coverImage.toString('base64')}`
     }
 })
 
